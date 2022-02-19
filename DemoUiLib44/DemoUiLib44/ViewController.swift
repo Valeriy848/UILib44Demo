56a0e81c88ff0cb770ee7a08cdbd552bf28f5478
v.pokatilo <valeriy.pokatilo@yandex.ru>
2022-02-19 13:55:11 +0700

    Создание проекта
    [DEMO-1]

---
+++ DemoUiLib44/DemoUiLib44/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  DemoUiLib44
+//
+//  Created by Valeriy on 19.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
