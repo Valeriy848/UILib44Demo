7838127e0085eca5b8dbfe564aa31a2ece968748
v.pokatilo <valeriy.pokatilo@yandex.ru>
2022-02-19 17:32:36 +0700

    Реализован BottomSheet
    [DEMO-2]

---
+++ DemoUiLib44/DemoUiLib44/Metrics.swift
@@ -0,0 +1,16 @@
+//
+//  Metrics.swift
+//  DemoUiLib44
+//
+//  Created by Valeriy on 19.02.2022.
+//
+
+import UIKit
+
+internal final class Metrics {
+    
+    private init() {}
+    
+    public static let cornerRadius: CGFloat = 12
+    public static let animationDuration: CGFloat = 0.3
+}
